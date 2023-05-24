2f1cf1e575bded1db7edc29a508436fc7223118a
Felipe A. Moreno <felipe@executive.com.br>
2023-05-24 09:30:22 -0300

    models

--- _utils.py
+++ _utils.py
@@ -30,6 +30,7 @@ def get_train_transform():
         A.HorizontalFlip(0.5),
         ToTensorV2(p=1.0)],
         bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
+
 def get_test_transform():
     return A.Compose([
         ToTensorV2(p=1.0)
